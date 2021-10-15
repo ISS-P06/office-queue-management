ec487af26a3f36cf7cdcd5ce918df4fe4b6495d9
Antonino Monti s287037 <s287037@studenti.polito.it>
2021-10-15 10:02:22 +0200

    Defined front-end routes
    Defined front-end routes, added AppNavbar component, defined login and logout front-end APIs

---
+++ client/src/api.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+
+const api_login = async (credentials) => {
+    try {
+        let res = await axios.post("/api/sessions", {
+                    username: credentials.username,
+                    password: credentials.password
+                }
+            );
+        if (res.data.username) {
+            return res.data;
+        }
+        else {
+            throw new Error(res.data.message);
+        }
+    }   
+    catch (err) {
+        if (err.response.status == 401) {
+            throw new Error(err.response.data);
+        }
+        else {
+            throw new Error("Sorry, there was an error in logging in");
+        }
+    }
+}
+
+const api_logout = () => {
+    axios.delete("/api/sessions/current")
+        .then((res) => {
+            // ...
+        }) 
+        .catch((res) => {
+            // ...
+        });
+}
+
+const api_getUserInfo = async () => {
+    try {
+        const res = await axios.get('/api/sessions/current');
+        if (res.data.id) {
+            return res.data;
+        }
+        else {
+            throw res.data.message;
+        }
+    }   
+    catch (err) {
+        if (err.response.data.message) {
+            throw new Error(err.response.data.message);
+        }
+        else if (err.response.data.error) {
+            throw new Error(err.response.data.error);
+        }
+        else {
+            throw new Error("Sorry, there was an error in logging in");
+        }
+    }
+}
+
+export {api_login, api_logout, api_getUserInfo};
