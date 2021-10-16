fdef230ab6357ae54b94b2f61fe84d8a33c213a6
ThePiromaximus <gabriele.inzerillo98@libero.it>
2021-10-16 14:36:37 +0200

    Revert "feat: implemented reset of queues and tickets"
    This reverts commit b95a1a0ec3a0aac2f15bfb5e9ac560016acdfd69.

--- server/dao.js
+++ server/dao.js
@@ -1,22 +1,3 @@
 'use strict';
 
 import db from './db';
-
-//Reset tickets and queues
-exports.reset = () => {
-    return new Promise((resolve, reject) => {
-      const sql = `
-                    UPDATE Ticket
-                    SET status = "not_served"
-                    WHERE status = "in_queue"
-                   `;
-      db.run(sql, [], err => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        console.log(`Tickets updated : ${this.changes}`);
-        resolve(this.changes);
-      });
-    });
-  }
