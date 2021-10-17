da7d9d1b9629b821b6dde9d6811e355051671a9f
_mary <mary_pal@live.it>
2021-10-17 10:15:38 +0200

    Update window

--- client/src/components/NextClientWindow.js
+++ client/src/components/NextClientWindow.js
@@ -1,4 +1,4 @@
-import { Navbar, Col, Button } from 'react-bootstrap';
+import { Navbar, Col, Button, Row } from 'react-bootstrap';
 import { useState } from 'react';
 import { api_callNextClient } from '../api';
 
@@ -13,7 +13,14 @@ function NextClientWindow(props) {
   };
 
   return (
-    <Button variant="success" size="lg" onClick={callNextClient}> Serve the next client </Button>
+    <Row className="justify-content-center"> 
+      <h1  className="mt-5"> Counter n°: {idCounter}</h1>
+      <h3>You are serving ticket n°: {idTicket}</h3>
+      <Col xs="6" md="3">
+      <Button  size="lg" className="mt-3" onClick={callNextClient}> Serve the next client </Button>
+      </Col>   
+    </Row>
+
   );
 }
 
